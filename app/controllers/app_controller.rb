0566a48680afa59011311a8f4f94decf29592ea9
jjhlzn <jinjunhang@hotmail.com>
2016-06-10 08:54:11 +0800

    update

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -12,7 +12,7 @@ class AppController < ApplicationController
   
   def getAds
     ads = []
-    ads.append({imageUrl: "http://img.weiphone.net/1/h061/h23/bc9c8fe1img201606071030220_306__220.jpg", clickUrl: "http://www.baidu.com", title: "信用卡活动"})
+    ads.append({imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPU-BPx2-AI-2RypBwN7LaTDiPhSSBrqSyMFfoC5agCEfcsNBK", clickUrl: "http://www.baidu.com", title: "信用卡活动"})
     ads.append({imageUrl: "http://pic.sc.chinaz.com/files/pic/pic9/201508/apic14052.jpg", clickUrl: "http://www.baidu.com", title: "刷卡活动"})
     render json: {status: 0, errorMessage: '', ads: ads}
   end
