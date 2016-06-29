4e64192553731f06d9a6806b22d4dc7350ef2987
jjhlzn <jinjunhang@hotmail.com>
2016-06-29 17:18:38 +0800

    update

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -7,8 +7,8 @@ class CommentController < ApplicationController
   
   def addLive 
     comments = []
-    comments.append({id: 1, userId: '那是当然', time: '1小时前', content: '11111'})
-    comments.append({id: 1, userId: 'frozenmoon', time: '1天前', content: '222'})
-    render json: {status: 0, errorMessage: '', comments: comments}
+    comments.append({id: 1, userId: '那是当然', name: 'jjh', time: '1小时前', content: '11111'})
+    comments.append({id: 1, userId: 'frozenmoon', name: 'jjh', time: '1天前', content: '222'})
+    render json: {status: 0, errorMessage: '', name: 'jjh', comments: comments}
   end
 end
