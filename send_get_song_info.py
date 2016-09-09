0e44d77ebdb6bdda8ae58fe6cbf5a9decee583eb
jjhlzn <jinjunhang@hotmail.com>
2016-09-09 20:59:19 +0800

    update

---
+++ send_get_song_info.py
@@ -0,0 +1,61 @@
+# coding:UTF-8
+import httplib
+import datetime
+import time
+import urllib
+import json
+import threading
+from time import ctime,sleep
+import smtplib
+from email.mime.text import MIMEText
+
+import sys
+
+HOST = "jf.yhkamani.com"
+
+def send_request():
+    conn = httplib.HTTPConnection(HOST)
+    jsondata = {"userInfo": {
+                 "token": "xxxxxxxxxx",
+                 "userid": "13706794299"},
+			"client": {
+                 'appversion': "1.0.3",
+                 'model': "iPhone6s",
+                 'osversion': "9.3",
+                 'platform': "iPhone",
+                 'screensize': "375*667"},
+            "request": {
+                 'id': "10"}				 }
+ 
+    try:
+        conn.request("POST", "/song/getsonginfo", json.dumps(jsondata), {"Content-type": "application/json"})
+		
+        res = conn.getresponse()
+        data = res.read()
+        print data
+        print "success!\n"
+    except Exception, e:
+        print e
+
+def main():
+    conn = httplib.HTTPConnection(HOST)
+    while(1):
+		try:
+			send_request()
+		except Exception, e:
+			print e
+			return
+		sleep(5)
+
+threads = []
+for i in range(300):
+    t1 = threading.Thread(target=main)
+    threads.append(t1)
+	
+if __name__ == '__main__':
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+    t.join()
+    print "all over %s" %ctime()
+	
